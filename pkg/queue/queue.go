6c4f655aa89b59792eb967a3c2395fa8f95e067a
m1khal3v <m1kh@l3v.moscow>
2024-08-10 14:27:23 +0300

    рефакторинг

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -9,6 +9,8 @@ type Queue[T any] struct {
 	items chan T
 }
 
+type removeBatchFilter[T any] func(items []T) error
+
 func New[T any](size uint64) *Queue[T] {
 	return &Queue[T]{
 		items: make(chan T, size),
@@ -17,6 +19,18 @@ func New[T any](size uint64) *Queue[T] {
 
 func (queue *Queue[T]) Push(item T) {
 	queue.items <- item
+}
+
+func (queue *Queue[T]) PushBatch(items []T) {
+	for _, item := range items {
+		queue.items <- item
+	}
+}
+
+func (queue *Queue[T]) PushChannel(items <-chan T) {
+	for item := range items {
+		queue.items <- item
+	}
 }
 
 func (queue *Queue[T]) PushDelayed(ctx context.Context, item T, delay time.Duration) {
@@ -37,6 +51,37 @@ func (queue *Queue[T]) Pop() (T, bool) {
 	default:
 		return *new(T), false
 	}
+}
+
+func (queue *Queue[T]) PopBatch(count uint64) []T {
+	if count == 0 || len(queue.items) == 0 {
+		return []T{}
+	}
+
+	items := make([]T, 0, count)
+
+	for i := uint64(0); i < count; i++ {
+		select {
+		case item := <-queue.items:
+			items = append(items, item)
+		default:
+			// no more items to return
+			return items
+		}
+	}
+
+	return items
+}
+
+func (queue *Queue[T]) RemoveBatch(count uint64, filter removeBatchFilter[T]) error {
+	items := queue.PopBatch(count)
+	if err := filter(items); err != nil {
+		queue.PushBatch(items)
+
+		return err
+	}
+
+	return nil
 }
 
 func (queue *Queue[T]) Count() uint64 {
